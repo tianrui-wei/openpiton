4476bfeb7d0ca4216121a6441ceed55689ab7164
Tianrui Wei <tianrui-wei@outlook.com>
2021-01-18 17:52:11 +0800

    quick hack to allow sim to load binary file

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-source piton/ariane_setup.sh
-cp /home/eva/piton/uboot/spl/u-boot-spl /home/eva/piton/piton_binary/u-boot-spl.riscv
-cd piton/build
+cp /home/eva/piton/uboot/spl/u-boot-spl.bin /home/eva/piton/piton_bin/u-boot-spl.riscv
+cd build
 sims -sys=manycore -vcs_build -debug_all -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
-sims -sys=manycore -vcs_run -gui -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
+#sims -sys=manycore -vcs_run -gui -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
+sims -sys=manycore -vcs_run -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
