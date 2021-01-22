c2497fd368ca35245be9bf41ef049a5a87bafe86
Tianrui Wei <tianrui-wei@outlook.com>
2021-01-22 23:59:32 +0800

    scripts for local testing

--- setup.bash
+++ setup.bash
@@ -3,4 +3,4 @@ cp /home/eva/piton/uboot/spl/u-boot-spl.bin /home/eva/piton/piton_bin/u-boot-spl
 cd build
 sims -sys=manycore -vcs_build -debug_all -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
 #sims -sys=manycore -vcs_run -gui -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
-sims -sys=manycore -vcs_run -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
+sims -sys=manycore -vcs_run -nosimslog -x_tiles=1 -y_tiles=1 u-boot-spl.riscv -ariane -precompiled -vcs_build_args="-full64"
