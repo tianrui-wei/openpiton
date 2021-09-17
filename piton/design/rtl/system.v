94a54c84e9d2fb0fcebc925cb1b85be7acb795da
Tianrui Wei <tianrui-wei@outlook.com>
2021-09-17 15:24:06 +0800

    [fix] fix unused bscan2 primitive in system.v
    Previously the bscane2 primitive in system.v is not used. Delete it to
    clean up
    
    Signed-off-by: Tianrui Wei <tianrui-wei@outlook.com>

--- piton/design/rtl/system.v
+++ piton/design/rtl/system.v
@@ -191,7 +191,7 @@ module system(
 `endif//XUPP3R_BOARD
 `endif //NEXYSVIDEO_BOARD
 `endif //VCU118_BOARD
-`endif //KC705_BOARD
+`endif  //KC705_BOARD
 `endif  //VC707_BOARD
 `endif //PITON_ARIANE
 `endif //PITON_FPGA_SYNTH
@@ -665,27 +665,9 @@ assign passthru_pll_rst_n = 1'b1;
         .TDO(td_o) // 1-bit input: Test Data Output (TDO) input for USER function.
     );
 `endif
-`ifdef KC705_BOARD
+
     wire tck_i, tms_i, trst_ni, td_i, td_o;
 
-    // hook the RISC-V JTAG TAP into the FPGA JTAG chain
-    BSCANE2 #(
-    .JTAG_CHAIN(1) // Value for USER command. Possible values: 1-4.
-    ) BSCANE2_inst (
-        .CAPTURE(), // 1-bit output: CAPTURE output from TAP controller.
-        .DRCK(), // 1-bit output: Gated TCK output. When SEL is asserted, DRCK toggles when CAPTURE or
-        // SHIFT are asserted.
-        .RESET(trst_ni), // 1-bit output: Reset output for TAP controller.
-        .RUNTEST(), // 1-bit output: Output asserted when TAP controller is in Run Test/Idle state.
-        .SEL(), // 1-bit output: USER instruction active output.
-        .SHIFT(), // 1-bit output: SHIFT output from TAP controller.
-        .TCK(tck_i), // 1-bit output: Test Clock output. Fabric connection to TAP Clock pin.
-        .TDI(td_i), // 1-bit output: Test Data Input (TDI) output from TAP controller.
-        .TMS(tms_i), // 1-bit output: Test Mode Select output. Fabric connection to TAP.
-        .UPDATE(), // 1-bit output: UPDATE output from TAP controller
-        .TDO(td_o) // 1-bit input: Test Data Output (TDO) input for USER function.
-    );
-`endif
 `ifdef NEXYSVIDEO_BOARD
     wire tck_i, tms_i, trst_ni, td_i, td_o;
 
